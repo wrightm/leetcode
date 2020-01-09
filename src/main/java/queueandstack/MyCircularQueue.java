d13671a1f088ded378f14fb45942a4b4372c9f7b
wrightm <wright1michael@gmail.com>
2020-01-09 22:09:58 +0000

    add initial circular queue

---
+++ src/main/java/queueandstack/MyCircularQueue.java
@@ -0,0 +1,51 @@
+package queueandstack;
+
+public class MyCircularQueue {
+
+    private final int[] items;
+    private int start;
+    private int end;
+    private int size;
+
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        this.items = new int[k];
+        this.start = 0;
+        this.end = 0;
+        this.size = 0;
+    }
+
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+
+    }
+
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+
+    }
+
+    /** Get the front item from the queue. */
+    public int Front() {
+
+    }
+
+    /** Get the last item from the queue. */
+    public int Rear() {
+
+    }
+
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+
+    }
+
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
