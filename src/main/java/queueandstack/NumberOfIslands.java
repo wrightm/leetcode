d54424bc98fb71bfbf6323a526a5cd32d1a7e7f8
wrightm <wright1michael@gmail.com>
2020-01-13 22:29:55 +0000

    add number of islands

--- src/main/java/queueandstack/NumberOfIslands.java
+++ src/main/java/queueandstack/NumberOfIslands.java
@@ -24,7 +24,74 @@ Output: 3
  */
 public class NumberOfIslands {
 
-    public static int numIslands(char[][] grid) {
 
+    public static int numIslands(char[][] grid) {
+        int nRows = grid.length;
+        int nColumns = grid[0].length;
+
+        int nIslands = 0;
+        /*
+        {'1','0','1','0','1'},
+        {'1','1','0','1','1'},
+        {'1','0','1','0','1'},
+        {'0','1','1','1','0'}
+         */
+        for(int i = 0; i < nRows; i++){
+            for(int j = 0; j < nColumns; j++){
+                char cell = grid[i][j];
+                if(cell == '1'){
+
+                }
+            }
+        }
+
+        return nIslands;
+    }
+
+    public static void main(String[] args){
+//        char[][] grid1 = {
+//                {'1','1','1','1','0'},
+//                {'1','1','0','1','0'},
+//                {'1','1','0','0','0'},
+//                {'0','0','0','0','0'}
+//        };
+//
+//        System.out.println(String.format("Expect 1 and got %s islands found.", numIslands(grid1)));
+//
+//        char[][] grid2 = {
+//                {'1','1','0','0','0'},
+//                {'1','1','0','0','0'},
+//                {'0','0','1','0','0'},
+//                {'0','0','0','1','1'}
+//        };
+//
+//        System.out.println(String.format("Expected 3 and got %s islands found.", numIslands(grid2)));
+//
+//        char[][] grid3 = {
+//                {'1','0','1','0','1'},
+//                {'1','0','1','0','1'},
+//                {'1','0','1','0','1'},
+//                {'1','0','1','0','1'}
+//        };
+//
+//        System.out.println(String.format("Expected 3 and got %s islands found.", numIslands(grid3)));
+//
+//        char[][] grid4 = {
+//                {'1','0','1','0','1'},
+//                {'0','1','0','1','0'},
+//                {'1','0','1','0','1'},
+//                {'0','1','0','1','0'}
+//        };
+//
+//        System.out.println(String.format("Expected 10 and got %s islands found.", numIslands(grid4)));
+
+        char[][] grid5 = {
+                {'1','0','1','0','1'},
+                {'1','1','0','1','1'},
+                {'1','0','1','0','1'},
+                {'0','1','1','1','0'}
+        };
+
+        System.out.println(String.format("Expected 4 and got %s islands found.", numIslands(grid5)));
     }
 }
