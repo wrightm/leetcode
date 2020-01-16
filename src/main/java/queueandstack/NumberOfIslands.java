c907d9b38bed6fd4c4ff18db5d27da1a6514d734
wrightm <wright1michael@gmail.com>
2020-01-16 06:28:32 +0000

    add number of islands example

--- src/main/java/queueandstack/NumberOfIslands.java
+++ src/main/java/queueandstack/NumberOfIslands.java
@@ -25,6 +25,113 @@ Output: 3
 public class NumberOfIslands {
 
     public static int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0){
+            return 0;
+        }
+        int nRows = grid.length;
+        int nColumns = grid[0].length;
 
+        int nIslands = 0;
+        for(int i = 0; i < nRows; i++){
+            for(int j = 0; j < nColumns; j++){
+                char cell = grid[i][j];
+                if(cell == '1') {
+                    if (!countedBefore(i, j, grid)) {
+                        nIslands++;
+                        markCells(i, j, grid);
+                    } else {
+                        markCells(i, j, grid);
+                    }
+                }
+            }
+        }
+        return nIslands;
+    }
+
+    public static boolean countedBefore(int i, int j, char[][] grid){
+        boolean counted = false;
+        int nRows = grid.length;
+        int nColumns = grid[0].length;
+        if(i-1 >= 0 && grid[i-1][j] == '-'){
+            counted = true;
+        }
+        if(i+1 < nRows && grid[i+1][j] == '-'){
+            counted = true;
+        }
+        if(j-1 >= 0 && grid[i][j-1] == '-'){
+            counted = true;
+        }
+        if(j+1 < nColumns && grid[i][j+1] == '-'){
+            counted = true;
+        }
+        return counted;
+    }
+
+    public static void markCells(int i, int j, char[][] grid){
+        int nRows = grid.length;
+        int nColumns = grid[0].length;
+        grid[i][j] = '-';
+        if(i-1 >= 0 && grid[i-1][j] == '1'){
+            markCells(i-1, j, grid);
+        }
+        if(i+1 < nRows && grid[i+1][j] == '1'){
+            markCells(i+1, j, grid);
+        }
+        if(j-1 >= 0 && grid[i][j-1] == '1'){
+            markCells(i, j-1, grid);
+        }
+        if(j+1 < nColumns && grid[i][j+1] == '1'){
+            markCells(i, j+1, grid);
+        }
+    }
+
+    public static void main(String[] args){
+        char[][] grid1 = {
+                {'1', '0', '1', '0', '1'},
+                {'1', '1', '0', '1', '1'},
+                {'1', '0', '1', '0', '1'},
+                {'0', '1', '1', '1', '0'},
+        };
+
+        System.out.println(String.format("Expected 4 and got %s islands", numIslands(grid1)));
+
+        char[][] grid2 = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'},
+        };
+
+        System.out.println(String.format("Expected 1 and got %s islands", numIslands(grid2)));
+
+        char[][] grid3 = {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1',},
+        };
+
+        System.out.println(String.format("Expected 3 and got %s islands", numIslands(grid3)));
+
+        char[][] grid4 = {
+                {'1','1','1','1','0'},
+                {'1','0','0','1','0'},
+                {'1','0','0','1','1'},
+                {'1','0','1','0','1'},
+        };
+
+        System.out.println(String.format("Expected 2 and got %s islands", numIslands(grid4)));
+
+        char[][] grid5 = {{}};
+
+        System.out.println(String.format("Expected 0 and got %s islands", numIslands(grid5)));
+
+        char[][] grid6 = {
+                {'1','0','1','1','1'},
+                {'1','0','1','0','1'},
+                {'1','1','1','0','1'}
+        };
+
+        System.out.println(String.format("Expected 1 and got %s islands", numIslands(grid6)));
     }
 }
